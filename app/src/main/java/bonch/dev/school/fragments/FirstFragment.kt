0036051ecacc36a5ab2d167494dd50b6f96a44b4
Nogginz <nogginz1488@gmail.com>
2019-10-18 17:01:32 +0300

    FINAL

--- app/src/main/java/bonch/dev/school/fragments/FirstFragment.kt
+++ app/src/main/java/bonch/dev/school/fragments/FirstFragment.kt
@@ -14,15 +14,15 @@ import bonch.dev.school.R
 
 
 class FirstFragment:Fragment(){
-    private lateinit var button1: Button
+    private lateinit var nextActivityButton: Button
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.first_fragment, container, false)
-        button1 = view.findViewById(R.id.next_activity_button)
-        button1.setOnClickListener(View.OnClickListener(){
+        nextActivityButton = view.findViewById(R.id.next_activity_button)
+        nextActivityButton.setOnClickListener(View.OnClickListener(){
             (activity as FragmentActivity).replace()
         })
         return view
